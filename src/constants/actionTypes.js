2147c8f3377569726240fc8d012884d61314fd3d
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-23 09:03:36 -0700

    added constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,19 +1,45 @@
-// Authentication constants
-export const AUTH_USER = 'AUTH_USER';
-export const UNAUTH_USER = 'UNAUTH_USER';
-export const AUTH_ERROR = 'AUTH_ERROR';
+//// Authentication constants
+//export const AUTH_USER = 'AUTH_USER';
+//export const UNAUTH_USER = 'AUTH_USER';
+//export const AUTH_ERROR = 'AUTH_USER';
+//
+//// Posts constants
+//export const FETCH_POSTS = 'FETCH_POSTS';
+//export const FETCH_POSTS_FAILED = 'FETCH_POSTS_FAILED';
+//export const SELECT_POST = 'SELECT_POST';
+//export const ADD_POST = 'ADD_POST';
+//
+//// User constants
+//export const GET_USER = 'GET_USER';
+//
+//// Query constants
+//export const UPDATE_QUERY = 'UPDATE_QUERY';
+//
+//// Clears the query reducer and selected post reducer;
+//export const CLEAR = 'CLEAR';
+const constants = [
+	'AUTH_USER',
+	'AUTH_USER',
+	'AUTH_USER',
+	'FETCH_POSTS',
+	'SELECT_POST',
+	'ADD_POST',
+	'GET_USER',
+	'UPDATE_QUERY',
+	'CLEAR',
+];
 
-// Posts constants
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const FETCH_POSTS_FAILED = 'FETCH_POSTS_FAILED';
-export const SELECT_POST = 'SELECT_POST';
-export const ADD_POST = 'ADD_POST';
+constants.forEach(type => {
+	if (type.endsWith('_')) {
+		Object.assign(exports, {
+			[`${type}STARTED`]: `${type}STARTED`,
+			[`${type}COMPLETED`]: `${type}COMPLETED`,
+			[`${type}FAILED`]: `${type}FAILED`,
+		});
+	} else {
+		Object.assign(exports, {
+			[type]: type,
+		});
+	}
+});
 
-// User constants
-export const GET_USER = 'GET_USER';
-
-// Query constants
-export const UPDATE_QUERY = 'UPDATE_QUERY';
-
-// Clears the query reducer and selected post reducer;
-export const CLEAR = 'CLEAR';
